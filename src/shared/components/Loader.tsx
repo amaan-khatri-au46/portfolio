c6aadb7803ac9f755f9c09f6cebd7aafdb60ac87
amaankhatri <kurehiamaan91@gmail.com>
2024-07-14 12:16:18 +0530

    changes done for loader

--- src/shared/components/Loader.tsx
+++ src/shared/components/Loader.tsx
@@ -1,20 +1,12 @@
-import React from "react";
 import { Html } from "@react-three/drei";
 
-interface LoaderProps {
-  size?: number;
-  color?: string;
-}
-
-export const Loader: React.FC<LoaderProps> = ({ size, color }) => {
+export const Loader = () => {
   return (
     <Html>
-      <div className="flex justify-center items-center">
-        <div
-          className={`w-${size} border-2 border-opacity-20 border-blue-500
-           border-t-blue-500 rounded-full animate-spin`}
-        />
+      <div className='flex justify-center items-center'>
+        <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
       </div>
     </Html>
   );
 };
+
